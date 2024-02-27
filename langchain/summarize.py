3d8baa2b3ca8f65138ea115bf0b3245a4e07b052
DavisAndn <davis.andherson@gmail.com>
2024-02-27 23:18:53 +0530

    feat(langchain): GPT4 test

--- langchain/summarize.py
+++ langchain/summarize.py
@@ -1,7 +1,7 @@
 import os
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI, ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import UnstructuredURLLoader
 import openai
@@ -26,9 +26,11 @@ docs = [Document(page_content=t) for t in texts[:]]
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # print(openai.api_key)
 
-llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0, openai_api_key=openai.api_key)
-map_reduce_chain = load_summarize_chain(llm, chain_type="map_reduce")
+# llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0, openai_api_key=openai.api_key)
+llm = ChatOpenAI(model_name="gpt-4", temperature=1)
+# map_reduce_chain = load_summarize_chain(llm, chain_type="map_reduce")
+gpt4_chain = load_summarize_chain(llm)
+# output = map_reduce_chain.invoke(docs)
+output = gpt4_chain.invoke(docs)
 
-output = map_reduce_chain.invoke(docs)
-
-print(output)
+print(output["output_text"])
