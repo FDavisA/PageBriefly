4cda257f7cb62d81b75e3b8c219c1ce5aa87891c
DavisAndn <davis.andherson@gmail.com>
2024-02-28 01:05:05 +0530

    feat(email): Add nltk tokenizer

--- utils/email_content.py
+++ utils/email_content.py
@@ -1,6 +1,10 @@
+from nltk.tokenize import sent_tokenize
+
 def generate_html_email_body(url, summary_text):
     introduction_sentence = f"The following is a brief of the given URL: {url}"
-    summary_as_bullets = "".join([f"<li>{sentence.strip()}</li>" for sentence in summary_text.split('\n') if sentence.strip() != ''])
+
+    sentences = sent_tokenize(summary_text)
+    summary_as_bullets = "".join([f"<li>{sentence.strip()}</li>" for sentence in sentences if sentence.strip() != ''])
 
     email_body = f"""
     <html>
