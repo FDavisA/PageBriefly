fc198f6e07b16639243d721b2ad8832e580f6e5f
DavisAndn <davis.andherson@gmail.com>
2024-02-28 00:05:32 +0530

    feat(gpt): Add gpt 3.5 + 4 support

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 URLS = ["https://www.storylane.io/"]
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MODEL_NAME = "gpt-4"
-
+# MODEL_NAME = "gpt-3.5-turbo-instruct"
 TEMPERATURE = 1
 CHUNK_SIZE = 3000
 CHUNK_OVERLAP = 200
